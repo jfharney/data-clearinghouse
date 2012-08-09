e9c3a685144f95346a401731096dadd360f4422c
Harney <8xo@lap00091497.gateway.2wire.net>
2012-08-09 02:33:11 -0400

    Added the Dataset data structure (replacing the Model data structure).  This data structure represents a single dataset in the ESGF repository

---
+++ src/java/main/org/esgf/dc/Dataset.java
@@ -0,0 +1,35 @@
+package org.esgf.dc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Dataset {
+
+	private String id;
+	private Map<String,String> metadata;
+	
+	public Dataset() {
+		this.setId(null);
+		this.setMetadata(new HashMap<String,String>());
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Map<String,String> getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(Map<String,String> metadata) {
+		this.metadata = metadata;
+	}
+	
+	
+	
+}
